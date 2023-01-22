25dce9b50ddcbd9cafec3b32924d7bcb6ea21ec3
Mani_Dev <dhingramsp@Gmail.com>
2023-01-23 00:03:13 +0530

    stuffs

--- src/index.js
+++ src/index.js
@@ -3,64 +3,63 @@ const path = require('path');
 let mainWindow;
 
 if (require('electron-squirrel-startup')) {
-  app.quit();
+	app.quit();
 }
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    icon: 'src/assets/calendar-white.png',
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-    },
-  });
+	mainWindow = new BrowserWindow({
+		icon: 'src/assets/icon.png',
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+		},
+	});
 
-  mainWindow.setMenu(null);
-  mainWindow.maximize();
-  mainWindow.loadFile(path.join(__dirname, 'public/index.html'));
+	mainWindow.setMenu(null);
+	mainWindow.maximize();
+	mainWindow.loadFile(path.join(__dirname, 'public/index.html'));
 
-  //mainWindow.webContents.openDevTools();
+	//mainWindow.webContents.openDevTools();
 
-  mainWindow.on('close', (event) => {
-    if (app.quitting) {
-      mainWindow = null
-    } else {
-      event.preventDefault()
-      mainWindow.hide()
-    }
-  })
+	mainWindow.on('close', (event) => {
+		if (app.quitting) {
+			mainWindow = null
+		} else {
+			event.preventDefault()
+			mainWindow.hide()
+		}
+	})
 };
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	tray.destroy();
+	app.quit()
 })
 
 app.on('activate', () => {
-  mainWindow.show()
+	mainWindow.show()
 });
 
 app.on('before-quit', () => {
-  app.quitting = true;
+	app.quitting = true;
 });
 
 let tray = null
 app.whenReady().then(() => {
-  tray = new Tray('src/assets/calendar-white.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
-  tray.addListener('click', () => {
-    mainWindow.show();
-  });
+	tray = new Tray('src/assets/icon.png')
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'Quit', type: 'normal', click: () => {
+			tray.destroy();
+			app.quit()
+		} }
+	])
+	tray.setToolTip('Sustainable Cities.')
+	tray.setContextMenu(contextMenu)
+	tray.addListener('click', () => {
+		mainWindow.show();
+	});
 })
